cad3db3f9804d9c401f24df2f26d95e9268ef965
Tijana Laporte Mitrovic <tijana.laportemitrovic@ouidou.fr>
2024-11-18 20:50:23 +0100

    init_back

---
+++ back/src/main/java/com/example/OpenAssoApp/OpenAssoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.OpenAssoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAssoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAssoAppApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/OpenAssoApp/OpenAssoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenAssoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAssoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
