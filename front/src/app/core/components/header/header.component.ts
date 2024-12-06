492de3b6b7868b614abf106ca2d4f928fd600519
Nicolas PERROTIN <106276527+nicotine189@users.noreply.github.com>
2024-12-06 11:45:09 +0100

    feature/OP-36: mise en place d'une palette de couleurs par défaut (#7)

--- front/src/app/core/components/header/header.component.ts
+++ front/src/app/core/components/header/header.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [MatIconModule],
+  imports: [
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    RouterModule
+  ],
   selector: 'app-header',
   standalone: true,
   styleUrl: './header.component.scss',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  isSidenavOpen = false;
 
+  toggleSidenav() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+  }
 }
