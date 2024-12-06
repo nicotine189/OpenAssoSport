57a960bb3f681ceaf309369ff6265c880d0d0f45
Nicolas PERROTIN <106276527+nicotine189@users.noreply.github.com>
2024-12-06 11:40:08 +0100

    Revert "feature/OP-36 palette de couleurs (#5)"
    This reverts commit 1f8a2aa7e56786be3517319f7293d7c814edbcd5.

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./core/components/header/header.component";
-import { FooterComponent } from './footer/footer.component'; // Importe le FooterComponent
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'open-asso-sport';
